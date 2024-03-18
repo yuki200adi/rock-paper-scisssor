21c5c79f679ec56f6dfd8f600f9ee146ebc32464
yuki200adi <144140894+yuki200adi@users.noreply.github.com>
2024-03-18 10:53:58 +0530

    Add files via upload

---
+++ sci.js
@@ -0,0 +1,66 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = ()=> {
+    const options = ["rock","paper","Scissor"];
+  const randIdx =   Math.floor(Math.random()*3);
+  return options[randIdx];
+};
+
+const drawGame = ()=>{
+
+    msg.innerText = "game was draw.play again";
+    msg.style.backgroundColor = "blue";
+
+}
+
+const showWinner = (userWin,userChoice,compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        
+        msg.innerText = "you win!";
+        msg.style.backgroundColor = "green";
+    }else {
+        compScore++;
+        compScorePara.innerText=compScore;
+        
+        msg.innerText = "you lose";
+        msg.style.backgroundColor = "red";
+    }
+}
+
+const playGame = (userChoice)=> {
+    
+    const compChoice = genCompChoice();
+     
+    console.log("comp choice = " ,compChoice);
+    if(userChoice === compChoice) {
+drawGame();
+    }else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            compChoice === "paper" ? false : true ;
+        } else if(userChoice === "paper") {
+          userWin =   compChoice === "scissors" ? false : true;
+        } else {
+            compChoice === "rock" ? false :true;
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+choice.addEventListener("click" , ()=> {
+    const userChoice = choice.getAttribute("id");
+    console.log("choice was clicked",userChoice);
+    playGame(userChoice);
+})
+
+});
